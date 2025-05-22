fbb28dd9a9b3e2db2d800c77e6a76f04c3653cc1
Byte-mastery <150344813+Byte-mastery@users.noreply.github.com>
2025-05-22 23:41:21 +0530

    Update script.js
    commited

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let currentScene = 0;
+const scenes = document.querySelectorAll('.scene');
 
 function nextScene() {
-  const scenes = document.querySelectorAll('.scene');
-  scenes[currentScene].classList.remove('active');
-  currentScene++;
-  if (currentScene < scenes.length) {
+  if (currentScene < scenes.length - 1) {
+    scenes[currentScene].classList.remove('active');
+    currentScene++;
     scenes[currentScene].classList.add('active');
+  } else {
+    document.getElementById("nextBtn").style.display = "none";
   }
 }
